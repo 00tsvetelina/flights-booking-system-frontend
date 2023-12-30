1efc0370f9833cbed8f9c436f6d593f1a30995e3
00tsvetelina <142497512+00tsvetelina@users.noreply.github.com>
2023-12-30 19:26:21 +0200

    added delete functionallity to flight

--- src/app/flight-list/flight-list.component.ts
+++ src/app/flight-list/flight-list.component.ts
@@ -2,16 +2,19 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, RouterLink } from '@angular/router';
 import { Flight } from '../flight';
 import { FlightService } from '../flight.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import { waitForAsync } from '@angular/core/testing';
+
 
 @Component({
   selector: 'app-flight-list',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatButtonModule, RouterLink, NgFor, CommonModule, MatPaginatorModule],
+  imports: [MatTableModule, MatIconModule, MatButtonModule, RouterLink, NgFor, CommonModule, MatPaginatorModule, MatSnackBarModule],
   templateUrl: './flight-list.component.html',
   styleUrl: './flight-list.component.css'
 })
@@ -21,7 +24,9 @@ export class FlightListComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   dataSource!: MatTableDataSource<Flight, MatPaginator>;
 
-  constructor(private flightService: FlightService){}
+  constructor(private flightService: FlightService,
+                private route: ActivatedRoute,
+                private matSnackBar: MatSnackBar){}
 
   ngOnInit(): void {
     this.flightService.getAllFlights().subscribe({
@@ -35,6 +40,20 @@ export class FlightListComponent implements OnInit, AfterViewInit {
         console.error('Cannot fetch data ', error);
       }}
     );
+  }
+
+  onDelete(flightId: number){
+    console.log(flightId);
+    this.flightService.deleteFlight(flightId).subscribe({
+      next: (result) => {
+        console.log("Deleted flight: ", result)
+        this.matSnackBar.open("Flight Deleted Successfully", "OK");
+        // window.location.reload();
+      },
+      error: (error) => {
+        console.error("Cannot delete flight with id: ", flightId)
+      }
+    })
   }
 
   ngAfterViewInit(): void {
--- src/app/flight.service.ts
+++ src/app/flight.service.ts
@@ -29,7 +29,11 @@ export class FlightService {
   updateFlight(flightId: number, flightData: Flight): Observable<Flight> {
     return this.httpClient.put<Flight>("http://localhost:8080/api/admin/flights/" + flightId, flightData);
   }
-  
+
+  // delete flight
+  deleteFlight(flightId: number): Observable<Flight> {
+    return this.httpClient.delete<Flight>("http://localhost:8080/api/admin/flights/" + flightId);    
+  }
 
 
 
